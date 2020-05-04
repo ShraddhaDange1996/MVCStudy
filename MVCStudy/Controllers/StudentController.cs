66cc552567507af6aa4ec2c241514d51102481a2
Shraddha.Dange <Shraddha.Dange@PNL-39CGTQ2>
2020-05-04 11:52:17 +0530

    Add project files.

---
+++ MVCStudy/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using MVCStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStudy.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var studentList = new List<StudentModel>
+            {
+                new StudentModel(){StudentId = 1 , StudentName ="Shraddha" , Age =22},
+                new StudentModel() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new StudentModel() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new StudentModel() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new StudentModel() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                new StudentModel() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                new StudentModel() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+            };
+            return View(studentList);
+        }
+        [HttpPost]
+        public ActionResult Edit(StudentModel std)
+        {
+            var id = std.StudentId;
+            var name = std.StudentName;
+            var age = std.Age;
+         //   var standardName = std.standard.StandardName;
+
+            return RedirectToAction("index");
+        }
+      
+    }
+}
---
+++ MVCStudy/Models/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCStudy.Models
+{
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+        [Display(Name="Name")]
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Standard
+    {
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+    }
+}
